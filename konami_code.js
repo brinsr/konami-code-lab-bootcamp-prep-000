a1af4f5cb30a6bceda32335b8c3ad76f01535f40
Brindha S <brinsree@gmail.com>
2018-05-05 01:02:40 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-function init() {
-  // Write your JavaScript code inside the init() function
-
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+function init(){
+  let index=0;
+ document.body.addEventListener('keydown',function(e){
+  
+  const entry=parseInt(e.detail||e.which);
+  if(entry===code[index]){
+    index++;
+    if(index===code.length-1){
+      alert("Congratulations!");
+      index=0;
+    } else{
+      index=0;
+    }
+  }
+},false);
 }
+  // Write your JavaScript code inside the init() function
+  
+ 
+
+
+
